b4dc255e5010bb303e7dea23daaf4c1a7a464c80
Warren R Bank <warren.r.bank@gmail.com>
2019-01-22 17:06:04 -0800

    bookmarks

---
+++ .channels/bookmarks.js
@@ -0,0 +1,33 @@
+// http://yoursports.stream/
+
+jQuery(document).ready(function($){
+  let data = []
+  let $a = $('ul#tV > li.ng-scope > div > a[ng-href]')
+
+  $a.each(function(){
+    let href = this.href
+    let name = this.innerText
+
+    data.push([href,name])
+  })
+
+  let md   = ''
+  let html = ''
+  let i
+
+  for (i=0; i<data.length; i++) {
+    let href = data[i][0]
+    let name = data[i][1]
+
+    md   += `  * [${name}](${href})\n`
+    html += `                <li><a href="${href}">${name}</a>\n`
+  }
+
+  console.log("\n----------------------------------------\n")
+  console.log('md:')
+  console.log(md)
+  console.log("\n----------------------------------------\n")
+  console.log('html:')
+  console.log(html)
+  console.log("\n----------------------------------------\n")
+})
