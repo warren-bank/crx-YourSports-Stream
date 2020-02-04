2bf147e003aa03ed8595112a554a8c52ae8407bf
Warren R Bank <warren.r.bank@gmail.com>
2020-02-04 00:59:19 -0800

    v0.2.0

--- YourSports-Stream/content.js
+++ YourSports-Stream/content.js
@@ -1,8 +1,31 @@
 var payload = function(){
-  var iframe_html = document.getElementById('player').outerHTML
+  const path  = window.location.pathname + window.location.search
+  const regex = {
+    iframe_holder: /^\/live(?:\.php)?\?v=/i,
+    iframe_page:   /^\/ing\//i
+  }
 
-  document.head.innerHTML = ''
-  document.body.innerHTML = iframe_html
+  if (regex.iframe_holder.test(path)) {
+    let iframe_html = document.getElementById('player').outerHTML
+
+    document.head.innerHTML = ''
+    document.body.innerHTML = iframe_html
+  }
+  else if (regex.iframe_page.test(path)) {
+    let iframe_holder = document.getElementById('player')
+
+    try {
+      ;[...iframe_holder.children].forEach(el => {
+        if (! el.hasAttribute('data-player')) el.remove()
+      })
+
+      // ================
+      // DOMException: play() failed because the user didn't interact with the document first
+      // ================
+      // iframe_holder.querySelector(':scope > [data-player] .player-poster.clickable').click()
+    }
+    catch(error){}
+  }
 }
 
 var inject_function = function(_function){
